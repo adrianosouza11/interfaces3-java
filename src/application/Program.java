1b7ed538e66b2b3e4b23d61cc1101d4a53c4290a
adrianosouza11 <adrianosouza11@gmail.com>
2018-10-28 04:26:55 -0200

    Solution with interface and class abstract

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,23 @@
 package application;
 
-import devices.Printer;
-import devices.Scanner;
+import devices.ComboDevice;
+import devices.ConcretePrinter;
+import devices.ConcreteScanner;
 
 public class Program {
     public static void main(String[] args){
-        Printer p = new Printer("1080");
+        ConcretePrinter p = new ConcretePrinter("1080");
         p.processsDoc("My Letter");
         p.print("My Letter");
-
-        Scanner s = new Scanner("2003");
+        System.out.println();
+        ConcreteScanner s = new ConcreteScanner("2003");
         s.processsDoc("My email");
+        System.out.println("Scan result: " + s.scan());
+
+        System.out.println();
+        ComboDevice c = new ComboDevice("2081");
+        c.processsDoc("My dissertation");
+        c.print("My dissertation");
         System.out.println("Scan result: " + s.scan());
     }
 }
---
+++ src/devices/ComboDevice.java
@@ -0,0 +1,22 @@
+package devices;
+
+public class ComboDevice extends Device implements Printer,Scanner{
+    public ComboDevice(String serialNumber) {
+        super(serialNumber);
+    }
+
+    @Override
+    public void processsDoc(String doc) {
+        System.out.println("Combo processing: " + doc);
+    }
+
+    @Override
+    public void print(String doc) {
+        System.out.println("Combo printing: " + doc);
+    }
+
+    @Override
+    public String scan() {
+        return "Combo scan result";
+    }
+}
